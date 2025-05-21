ff224da06b012b08504cd4c4802c12d70cab2232
Hackdogen <inanis004@gmail.com>
2025-05-21 13:25:59 +0800

    notes for web.php

--- routes/web.php
+++ routes/web.php
@@ -4,32 +4,37 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SellerProfileController;
 
+//returns to index page
 Route::get('/', function () {
     return view('welcome');
 });
 
+//bridge client to client dashboard
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+//bridge client to client profile
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-
+//bridge seller account to seller profile
 Route::middleware('auth:seller')->prefix('seller')->name('seller.')->group(function () {
     Route::get('/profile', [SellerProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [SellerProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [SellerProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+//page connectors for navlinks
 Route::get('welcome', function () {  return view('welcome');})->name('welcome');
 Route::get('/shop', function () {  return view('shop');})->name('shop');
 Route::get('/prototype', function () {  return view('prototype');})->name('prototype');
 Route::get('/comission', function () {  return view('comission');})->name('comission');
 require __DIR__.'/auth.php';
 
-
+// bridge seller to seller dashboard
 Route::get('/sellerdashboard', function () {
     return view('sellerdashboard');
 })->middleware('auth:seller')->name('sellerdashboard');
