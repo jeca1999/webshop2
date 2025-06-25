2f371c4133fb54cd281243c0cc866102c7e80e80
jeca1999 <angananganjennica461@gmail.com>
2025-06-25 16:07:33 +0800

    debug

--- resources/views/sellerdashboard.blade.php
+++ resources/views/sellerdashboard.blade.php
@@ -1,3 +1,5 @@
+@php dd('dashboard loaded'); @endphp
+@php dd(auth('seller')->check()); @endphp
 <x-app-layout>
     <!-- Header -->
     <header class="w-full px-2 sm:px-4 py-4 flex flex-col md:flex-row items-center justify-between gap-4 border-b border-gray-200 dark:border-gray-800 bg-white dark:bg-black">
