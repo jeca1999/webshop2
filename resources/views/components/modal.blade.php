98827b6668a5520cce23929ecf730365258f9cf1
jeca1999 <angananganjennica461@gmail.com>
2025-06-26 14:24:56 +0800

    modal fix- i wasnt asking

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -1,7 +1,7 @@
 @props([
     'name',
     'show' => false,
-    'maxWidth' => '2xl'
+    'maxWidth' => '3xl'
 ])
 
 @php
@@ -11,6 +11,8 @@ $maxWidth = [
     'lg' => 'sm:max-w-lg',
     'xl' => 'sm:max-w-xl',
     '2xl' => 'sm:max-w-2xl',
+    '3xl' => 'sm:max-w-3xl',
+    '4xl' => 'sm:max-w-4xl',
 ][$maxWidth];
 @endphp
 
@@ -50,7 +52,7 @@ $maxWidth = [
     <div
         x-show="show"
         class="fixed inset-0 transform transition-all"
-        x-on:click="show = false"
+     
         x-transition:enter="ease-out duration-300"
         x-transition:enter-start="opacity-0"
         x-transition:enter-end="opacity-100"
