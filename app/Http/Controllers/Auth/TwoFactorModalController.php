fb6bc4e34f8bbf53aa2a9fd7b1399b1a0521c59f
jeca1999 <angananganjennica461@gmail.com>
2025-06-26 23:58:12 +0800

    fix: remove unnecessary return type declaration from store method in TwoFactorModalController

--- app/Http/Controllers/Auth/TwoFactorModalController.php
+++ app/Http/Controllers/Auth/TwoFactorModalController.php
@@ -5,11 +5,10 @@ namespace App\Http\Controllers\Auth;
 use Illuminate\Support\Facades\Session;
 use Laravel\Fortify\Http\Controllers\TwoFactorAuthenticatedSessionController as FortifyTwoFactorController;
 use Laravel\Fortify\Http\Requests\TwoFactorLoginRequest;
-use Illuminate\Http\RedirectResponse;
 
 class TwoFactorModalController extends FortifyTwoFactorController
 {
-    public function store(TwoFactorLoginRequest $request): RedirectResponse
+    public function store(TwoFactorLoginRequest $request)
     {
         $response = parent::store($request);
         // Only clear the session flag if the code was correct (redirect is NOT back)
