258997c17600dc8f4af55698b85906222d2e37da
Gerrard Allidem <angananganjennica461@gmail.com>
2025-07-03 01:34:11 +0800

    raate limiter

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -46,7 +46,7 @@
         </div>
     @endif
 
-    <form method="POST" action="{{ secure_url('login') }}">
+    <form method="POST" action="{{ route('login') }}">
         @csrf
 
         <!-- Email Address -->
