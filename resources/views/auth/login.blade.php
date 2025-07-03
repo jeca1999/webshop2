3239b8e47598660e2a86edc8a3a5682d3d820859
Gerrard Allidem <angananganjennica461@gmail.com>
2025-07-03 09:08:31 +0800

    xss

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -51,7 +51,7 @@
 
         <!-- Email Address -->
         <div>
-            <x-input-label <script>alert('XSS');</script> for="email" :value="__('Email')" />
+            <x-input-label  for="email" :value="__('Email')" />
             <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
            
         </div>
